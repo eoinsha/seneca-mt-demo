5717df22516e3ed30c6c332fb2f99938018865bb
Eoin Shanaghy <eoin@edappy.com>
2015-11-26 16:58:52 +0000

    Mongo Store Interceptor

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 'use strict'
+const _ = require('lodash')
 const seneca = require('seneca')()
 const senecaContext = require('seneca-context')
 
+// Seneca context to set tenant in context from HTTP host
 seneca.use(require('seneca-context/plugins/setContext'), {createContext: (req, res, context, done) => {
   const tenant = req.hostname.match(/([^\.]+)/)[1]
   setImmediate(() => done(null, {tenant}))
 }})
 
+// Blog microservice
 seneca.add({role:'api', cmd:'post'}, function (args, done) {
-  const si = this
-  console.log('Context is', senecaContext.getContext(si).tenant)
-  si.make('blog/posts').save$(args.post, done)
+  this.make('blog/posts').save$(args.post, done)
 })
 
+// Blog API
 seneca.act({role:'web', use:{
   prefix: '/api/',
   pin: {role:'api', cmd:'*'},
-  map: {
-    'post': {GET: true}
-  }
+  map: {'post': {GET: true}}
 }})
 
-const app = require('express')()
-const bodyparser = require('body-parser')
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true }))
-app.use(seneca.export('web'))
-app.listen(8020)
+// Tenant Registry
+const opts = { host: '127.0.0.1', port: 27017 };
+['a','b','c','d'].forEach(
+    (tenant) => { seneca.use(`mongo-store$${tenant}`, _.assign(opts, {name: tenant, map:{[`${tenant}/-/-`]:'*'}})) }
+)
+
+// Store Interceptor
+seneca.ready(() => {
+  seneca.wrap({role:'entity'}, function (interceptedMessage, done) {
+    const seneca = this
+    const message = seneca.util.clean(interceptedMessage)
+    if (_.isUndefined(message.zone) && !(message.base === 'sys' && message.name === 'entity')) {
+      const data = message.ent.data$()
+      const context = senecaContext.getContext(seneca)
+      const canon = message.ent.canon$({object: true})
+      message.ent = seneca.make$(context.tenant, canon.base, canon.name).data$(data)
+      message.qent = message.qent && message.ent
+      message.zone = context.tenant
+    } 
+    return seneca.prior(message, done)
+  })
+  // Application Server
+  const app = require('express')()
+  const bodyparser = require('body-parser')
+  app.use(bodyparser.json())
+  app.use(seneca.export('web'))
+  app.listen(8020)
+})
